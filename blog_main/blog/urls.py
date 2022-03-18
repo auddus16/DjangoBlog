90312d2894025ce51b717965de6bef2e03a90a59
auddus16 <auddus16@naver.com>
2022-03-18 17:11:44 +0900

    블로그를 CBV로 리팩토링

--- blog_main/blog/urls.py
+++ blog_main/blog/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<int:pk>/', views.single_post_page),  # <자료형:변수명> -> single_post_page의 매개변수로 넘어간다.
-    path('', views.index)
+    path('', views.PostList.as_view()),
+    # path('<int:pk>/', views.single_post_page),  # <자료형:변수명> -> single_post_page의 매개변수로 넘어간다.
+    path('<int:pk>/', views.PostDetail.as_view()),  # <자료형:변수명> -> single_post_page의 매개변수로 넘어간다.
+    # path('', views.index)
+
 ]
