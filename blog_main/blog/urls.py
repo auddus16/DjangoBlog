2fec49bf33c0d9004cecc391f4a4020fbd6196f7
auddus16 <auddus16@naver.com>
2022-03-18 16:42:32 +0900

    blog 상세페이지 작성

--- blog_main/blog/urls.py
+++ blog_main/blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('<int:pk>/', views.single_post_page),  # <자료형:변수명> -> single_post_page의 매개변수로 넘어간다.
     path('', views.index)
 ]
