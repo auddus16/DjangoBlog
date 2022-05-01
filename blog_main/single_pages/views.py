b6fd55fd703f195877f3d0002d6252bd871f4759
auddus16 <auddus16@naver.com>
2022-05-01 23:18:26 +0900

    Comment오류수정

--- blog_main/single_pages/views.py
+++ blog_main/single_pages/views.py
@@ -8,7 +8,7 @@ def landing(request):
     recent_posts = Post.objects.order_by('-pk')[:3]
     return render(request, 'single_pages/landing.html',
                   {
-                      'recent_posts' : recent_posts
+                      'recent_posts' : recent_posts,
                   })
 
 
